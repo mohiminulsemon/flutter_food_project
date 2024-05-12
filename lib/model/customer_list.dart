fe6d34c4fdd296d88007f308e3ca944259b6f470
mohiminulsemon <mohiminulsemon80@gmail.com>
2024-05-12 11:56:48 +0600

    model changed

--- lib/model/customer_list.dart
+++ lib/model/customer_list.dart
@@ -2,24 +2,9 @@ List<Map<String, dynamic>> customerList() {
   return [
     {
       'image':
-          'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg',
-      'name': 'shimon',
-      'price': 25.00,
-      'rating': 4,
-      'time': '20'
-    },
-    {
-      'image':
-          'https://images.pexels.com/photos/1448721/pexels-photo-1448721.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=252&fit=crop&h=408',
-      'name': 'Rakib',
-      'price': 28.00,
-      'rating': 4.5,
-      'time': '20'
-    },
-    {
-      'image':
           'https://images.pexels.com/photos/842571/pexels-photo-842571.jpeg?auto=compress&cs=tinysrgb&w=600',
-      'name': 'sumon',
+      'name': 'Salad',
+      'type': 'Salads',
       'price': 25.00,
       'rating': 3,
       'time': '20'
@@ -27,7 +12,8 @@ List<Map<String, dynamic>> customerList() {
     {
       'image':
           'https://images.pexels.com/photos/699953/pexels-photo-699953.jpeg?auto=compress&cs=tinysrgb&w=600',
-      'name': 'arafat',
+      'name': 'Juice',
+      'type': 'Drinks',
       'price': 30.00,
       'rating': 4,
       'time': '20'
@@ -35,23 +21,8 @@ List<Map<String, dynamic>> customerList() {
     {
       'image':
           'https://images.pexels.com/photos/699953/pexels-photo-699953.jpeg?auto=compress&cs=tinysrgb&w=600',
-      'name': 'shimon',
-      'price': 25.00,
-      'rating': 4,
-      'time': '20'
-    },
-    {
-      'image':
-          'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg',
-      'name': 'shimon',
-      'price': 25.00,
-      'rating': 4,
-      'time': '20'
-    },
-    {
-      'image':
-          'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg',
-      'name': 'shimon',
+      'name': 'Sauce',
+      'type': 'Sauce',
       'price': 25.00,
       'rating': 4,
       'time': '20'
@@ -59,31 +30,17 @@ List<Map<String, dynamic>> customerList() {
     {
       'image':
           'https://images.pexels.com/photos/1448721/pexels-photo-1448721.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=252&fit=crop&h=408',
-      'name': 'Rakib',
+      'name': 'Rice Bowl',
+      'type': 'Set Menu',
       'price': 28.00,
       'rating': 4.5,
       'time': '20'
     },
     {
       'image':
-          'https://images.pexels.com/photos/842571/pexels-photo-842571.jpeg?auto=compress&cs=tinysrgb&w=600',
-      'name': 'sumon',
-      'price': 25.00,
-      'rating': 3,
-      'time': '20'
-    },
-    {
-      'image':
-          'https://images.pexels.com/photos/699953/pexels-photo-699953.jpeg?auto=compress&cs=tinysrgb&w=600',
-      'name': 'arafat',
-      'price': 30.00,
-      'rating': 4,
-      'time': '20'
-    },
-    {
-      'image':
-          'https://images.pexels.com/photos/699953/pexels-photo-699953.jpeg?auto=compress&cs=tinysrgb&w=600',
-      'name': 'shimon',
+          'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg',
+      'name': 'Burger',
+      'type': 'Fast Food',
       'price': 25.00,
       'rating': 4,
       'time': '20'
@@ -91,7 +48,8 @@ List<Map<String, dynamic>> customerList() {
     {
       'image':
           'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg',
-      'name': 'shimon',
+      'name': 'Cake',
+      'type': 'Dessert',
       'price': 25.00,
       'rating': 4,
       'time': '20'
